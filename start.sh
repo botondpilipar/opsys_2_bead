32ba26d69e372ba5ac99cde53ee6efd67eadde94
pili <pilipar.botond@gmail.com>
2020-05-06 07:51:13 +0200

    Shell script to reset file states is created to battle existing bugs and to do a fresh start.

--- start.sh
+++ start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+echo \
+"Hosszu
+horolas
+rugyfakaszto permetezes
+mettszes
+tavaszi nyitas
+Malom telek
+horolas
+rugyfakaszto permetezes
+mettszes
+tavaszi nyitas
+Szula
+horolas
+tavaszi nyitas
+rugyfakaszto permetezes
+mettszes
+Malom telek
+horolas
+tavaszi nyitas
+rugyfakaszto permetezes
+mettszes
+" \
+> resources/vineyard_tasks.txt
+
 rm resources/entries.bin
 touch resources/entries.bin
 
